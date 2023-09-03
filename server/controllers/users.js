4307f0b6ef891291cece99c8f3529a3b644cccaf
vviveksharma <visharma@progress.com>
2023-09-03 15:31:43 +0530

    User Auth backend
    Signed-off-by: vviveksharma <visharma@progress.com>

---
+++ server/controllers/users.js
@@ -0,0 +1,41 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import User from "../models/user.js";
+// register User
+
+export const register = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    console.log(req.body)
+    const salt = await bcrypt.genSalt();
+    const passHash = await bcrypt.hash(password, salt);
+    const NewUser = new User({
+      username,
+      email,
+      password: passHash,
+    });
+    const savedUser = await NewUser.save();
+    res.status(200).json(savedUser);
+  } catch (err) {
+    res.status(404).json({ error: err.message });
+  }
+};
+
+//logging In
+
+export const login = async(req, res) => {
+  try {
+    const {email, password} = req.body;
+    const user = await User.findOne({email:email})
+    if(!user) return res.status(400).json({msg: "User doesnot exist"})
+
+    const isMatch = await bcrypt.compare(password, user.password)
+    if(!isMatch) return res.status(400).json({msg:"The password entered is incorrect"})
+
+    const token = jwt.sign({id: user._id}, process.env.SECRET_KEY)
+    delete user.password
+    res.status(200).json({token, user})
+  }catch(err) {
+    res.status(500).json({err: err.message})
+  }
+}
---
+++ server/index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import { login, register } from "./controllers/users.js";
+const app = express();
+// parse application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: false }))
+     
+// parse application/json
+app.use(express.json())
+dotenv.config();
+app.get("/", (req, res) => {
+  res.send("User Authentication");
+});
+
+// ROUTES
+
+app.post("/register", register);
+app.post("/login", login)
+
+mongoose.connect(process.env.MONGO_URL , {
+    useNewUrlParser : true,
+    useUnifiedTopology : true,
+    })
+    .then(() => console.log("DataBase connection Successfull"))
+.catch((err) => console.log(err));
+
+app.use(express.json());
+
+app.listen(8000, () => {
+  console.log("Server is active");
+});
+
